319b854a5f071bfbae0e78fede1fb19ceb86f154
Kuralbek798 <agubaev_kuralbek@mail.ru>
2022-04-01 22:27:10 +0600

    01042022

---
+++ src/com/thebyteguru/main/Main.java
@@ -0,0 +1,22 @@
+package com.thebyteguru.main;
+
+import com.thebyteguru.display.Display;
+
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class Main {
+    public static void main(String[]args){
+        Display.create(800,600, "Tanks");
+
+        Timer t = new Timer(1000/60, new AbstractAction(){
+            public void actionPerformed(ActionEvent e){
+                  Display.render();
+            }
+        });
+       t.setRepeats(true);
+       t.start();
+        //
+    }
+}
